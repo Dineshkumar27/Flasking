2ed1ef80442a05e7de48cbe3ef78028ad4a73e8e
dkumar <dkumar.270985@gmail.com>
2020-07-09 18:15:46 +0530

    Form Creation and Validation

--- Hello.py
+++ Hello.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, flash,redirect
 
+from forms import Registrationform,Loginform
+app = Flask(__name__)
+app.config['SECRET_KEY']='6a6bed92766c1a7fe4a48e1c6c1aa564'
 posts=[
     {
         'author':'Dinesh Kumar',
@@ -17,7 +19,7 @@ posts=[
 ]
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html',posts=posts)
 
 
@@ -25,6 +27,27 @@ def hello_world():
 def about():
     return render_template('about.html',title="About us")
 
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=Registrationform()
+    if form.validate_on_submit():
+        flash(f'Account Created for the user {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="Registration",form=form)
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=Loginform()
+    if form.validate_on_submit():
+        if form.email.data=='dkumar.270985@gmail.com' and form.password.data=='dinesh1342':
+            flash(f'Welcome {form.email.data}!!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Check your username and password', 'danger')
+
+
+    return render_template('login.html',title="Registration",form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
